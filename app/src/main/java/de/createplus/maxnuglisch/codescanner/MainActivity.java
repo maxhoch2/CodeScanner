ed9bdc8b4e6c2080b320a5cebad23ec5ac200ebf
maxhoch2 <max.nuglisch@gmx.de>
2018-07-02 17:37:44 +0200

    ich hab da mal was gebastelt

--- app/src/main/java/de/createplus/maxnuglisch/codescanner/MainActivity.java
+++ app/src/main/java/de/createplus/maxnuglisch/codescanner/MainActivity.java
@@ -1,17 +1,19 @@
 package de.createplus.maxnuglisch.codescanner;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseArray;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -19,12 +21,24 @@ import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     SurfaceView cameraView;
     TextView textView;
     CameraSource cameraSource;
+    TextRecognizer textRecognizer;
+    String[] brands = {"GAFFELS", "FLIMM", "JAGERMEI", "REISSDORF"};
+    int[] brandIDS = {R.drawable.gaffels, R.drawable.flimm, R.drawable.jaegermeister, R.drawable.reisendorf};
+    String[] brandCodeDef = {"9n", "5n", "6", "7"}; // n=only numbers
+
+    String currentCode="";
+
+    int state = 0; // 0=scan brand // 1=scan code // 2= locked
+
+    int currentBrand = -1;
+    ArrayList<String> recognizedBrands = new ArrayList<String>();
     final int RequestCameraPermissionID = 1001;
 
     @Override
@@ -63,7 +77,24 @@ public class MainActivity extends AppCompatActivity {
 
         cameraView = (SurfaceView) findViewById(R.id.surface_view);
         textView = (TextView) findViewById(R.id.text_view);
-        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        setupCamera();
+        ImageButton btn = (ImageButton) findViewById(R.id.var_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "state:" + state, Toast.LENGTH_SHORT).show();
+                if (state == 0 && currentBrand != -1) {
+
+                    state = 1;
+                    ImageButton btn = (ImageButton) findViewById(R.id.var_button);
+                    btn.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+
+    public void setupCamera() {
         if (!textRecognizer.isOperational()) {
             Log.w("MainActivity", "Detector not available");
         } else {
@@ -78,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
                 public void surfaceCreated(SurfaceHolder surfaceHolder) {
                     try {
                         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},RequestCameraPermissionID);
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
                             return;
                         }
                         cameraSource.start(cameraView.getHolder());
@@ -107,17 +138,50 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void receiveDetections(Detector.Detections<TextBlock> detections) {
                     final SparseArray<TextBlock> items = detections.getDetectedItems();
-                    if(items.size() != 0){
+                    if (items.size() != 0) {
                         textView.post(new Runnable() {
                             @Override
                             public void run() {
                                 StringBuilder stringBuilder = new StringBuilder();
-                                for(int i = 0; i < items.size();i++){
+                                for (int i = 0; i < items.size(); i++) {
                                     TextBlock item = items.valueAt(i);
                                     stringBuilder.append(item.getValue());
-                                    stringBuilder.append("\n");
+                                    //stringBuilder.append("\n");
                                 }
-                                textView.setText(stringBuilder.toString());
+                                String in = stringBuilder.toString();
+
+                                if (state == 0) {
+                                    for (int i = 0; i < brands.length; i++) {
+                                        if (in.toUpperCase().replace(" ", "").contains(brands[i])) {
+                                            recognizedBrands.add(brands[i]);
+
+                                            ImageButton btn = (ImageButton) findViewById(R.id.var_button);
+                                            btn.setImageResource(brandIDS[i]);
+                                            currentBrand = i;
+
+
+                                        }
+                                    }
+                                }
+
+                                if (state == 1) {
+                                    String filteredIN = "";
+                                    if (brandCodeDef[currentBrand].contains("n")) {
+                                        filteredIN = in.replaceAll("[^0-9]", "");
+                                    } else {
+                                        filteredIN = in;
+                                    }
+
+                                    textView.setText("Scanne den Code:\n" + filteredIN);
+                                    textView.setTextColor(Color.WHITE);
+
+                                    if(filteredIN.length() == Integer.parseInt(""+brandCodeDef[currentBrand].charAt(0))){
+                                        textView.setText("Code:\n" + filteredIN);
+                                        textView.setTextColor(Color.GREEN);
+                                    }
+                                }
+
+
                             }
                         });
                     }
@@ -125,6 +189,4 @@ public class MainActivity extends AppCompatActivity {
             });
         }
     }
-
-
 }
